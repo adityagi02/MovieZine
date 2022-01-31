b36c41c989b5550dc6a9063b0a4faed0abe62cbb
adityagi02 <adityatyagi20bcs126@iiitkottayam.ac.in>
2022-01-31 16:02:28 +0530

    Initial Commit

---
+++ Moviezine/MoviezineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviezineApp.swift
+//  Moviezine
+//
+//  Created by Aditya Tyagi  on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviezineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
